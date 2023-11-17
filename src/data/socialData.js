87880d8cc2612589846d85f21ae7ab686f9a8a5c
skaroweb <skaroweb@gmail.com>
2023-11-17 16:16:06 +0530

    third

--- src/data/socialData.js
+++ src/data/socialData.js
@@ -2,7 +2,7 @@ import {
   faFacebookSquare,
   faLinkedin,
   faGithubSquare,
-  faTwitterSquare,
+  faXTwitter,
   faInstagramSquare,
 } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
@@ -12,42 +12,42 @@ let SocialData = [
     socialMediaID: 1,
     socialMediaName: "Facebook",
     socialMediaIcon: faFacebookSquare,
-    socialMediaLink: "https://www.facebook.com/",
+    socialMediaLink: "https://www.facebook.com/mithun.awesome/",
   },
+
+  // {
+  //   socialMediaID: 2,
+  //   socialMediaName: "Linkedin",
+  //   socialMediaIcon: faLinkedin,
+  //   socialMediaLink: "https://www.linkedin.com/",
+  // },
 
   {
     socialMediaID: 2,
-    socialMediaName: "Linkedin",
-    socialMediaIcon: faLinkedin,
-    socialMediaLink: "https://www.linkedin.com/",
-  },
-
-  {
-    socialMediaID: 3,
     socialMediaName: "Github",
     socialMediaIcon: faGithubSquare,
-    socialMediaLink: "https://github.com/",
-  },
-
-  {
-    socialMediaID: 1,
-    socialMediaName: "Twitter",
-    socialMediaIcon: faTwitterSquare,
-    socialMediaLink: "https://www.twitter.com/",
-  },
-
-  {
-    socialMediaID: 2,
-    socialMediaName: "Instagram",
-    socialMediaIcon: faInstagramSquare,
-    socialMediaLink: "https://www.instagram.com/",
+    socialMediaLink: "https://github.com/mithunkumar0604",
   },
 
   {
     socialMediaID: 3,
+    socialMediaName: "Twitter",
+    socialMediaIcon: faXTwitter,
+    socialMediaLink: "https://twitter.com/mithunkumar0604",
+  },
+
+  {
+    socialMediaID: 4,
+    socialMediaName: "Instagram",
+    socialMediaIcon: faInstagramSquare,
+    socialMediaLink: "https://www.instagram.com/mithu.cookie.monster/",
+  },
+
+  {
+    socialMediaID: 5,
     socialMediaName: "Email",
     socialMediaIcon: faEnvelope,
-    socialMediaLink: "mailto:xyz@yahoo.com",
+    socialMediaLink: "mailto:skarosoft@gmail.com",
   },
 ];
 
